ce98575071b0c6193b9a44023a0c7e55c8ed226f
Ana Beatriz <65046952+bianatris@users.noreply.github.com>
2020-06-11 23:53:27 -0300

    Update main.py
    Jorge tentou adicionar o menu

--- main.py
+++ main.py
@@ -14,6 +14,28 @@ screen = pygame.display.set_mode((800, 600))  # ((width, height))  ((x, y))
 #       |
 #       V 600 px
 
+
+# -=-=-=-= FONTE MENU -=-=-=-= #
+def escrever_texto (text, font, color, suface, x, y):
+    textobj = font.render(text, 1, color)
+
+
+import pygame
+import random
+
+# inicializando o pygame
+pygame.init()
+
+# =-=-=-=-=-=-=-=-=-= TAMANHO DA TELA =-=-=-=-=-=-=-=-=-= #
+screen = pygame.display.set_mode((800, 600))  # ((width, height))  ((x, y))
+#
+#       -------------------- >
+#       |               800 px
+#       |
+#       |
+#       |
+#       V 600 px
+
 # =-=-=-=-=-=-=-=-=-= BACKGROUND =-=-=-=-=-=-=-=-=-= #
 background = pygame.transform.scale(pygame.image.load('fundo.png'),(800,600))
 
@@ -119,6 +141,36 @@ def colisao_chao (ya,yb) :
         return True
     else:
         return False
+
+# =-=-=-= GAME MENU =-=-=-= #
+def menu_do_jogo():
+    while True:
+
+        screen.fill((255,255,255))
+        escrever_texto('Go Gary', font, (0, 0, 255), screen, 400, 30)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(400, 250, 300, 100)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game_loop()
+        pygame.draw.rect(sreen, (0,0,0), button_1)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        pygame.display.update()
+        mainClock.tick(60)
 
 # =-=-=-= GAME LOOP =-=-=-= #
 running = True
@@ -240,3 +292,4 @@ while running:
 
     # nada aparece se não tiver a função update
     pygame.display.update()
+    mainClock.tick(60)
